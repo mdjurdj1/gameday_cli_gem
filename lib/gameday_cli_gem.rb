75be3150fbc1e46b906d92f0ad1f0cc26a12bd35
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-02-11 01:47:04 +0000

    Addded Scraper class

--- lib/gameday_cli_gem.rb
+++ lib/gameday_cli_gem.rb
@@ -5,6 +5,7 @@ require 'nokogiri'
 require_relative "./gameday_cli_gem/version"
 require_relative "./gameday_cli_gem/cli"
 require_relative "./gameday_cli_gem/game"
+require_relative "./gameday_cli_gem/scraper"
 
 module GamedayCliGem
 end
