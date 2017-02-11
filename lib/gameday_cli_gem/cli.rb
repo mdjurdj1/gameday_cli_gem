d8924fafc802f27228ab696eaac6d88906856fa9
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-02-11 01:04:37 +0000

    Stubed out list_games method for CLI.rb

--- lib/gameday_cli_gem/cli.rb
+++ lib/gameday_cli_gem/cli.rb
@@ -5,4 +5,11 @@ class GamedayCliGem::CLI
      menu
      goodbye
    end
+
+ def list_games  #method will list all of the day's games, including score for completed/in-progress games
+   puts "Today's Upcoming Games:"
+   @games = Gameday::Games.today
+ end
+
+   
 end
