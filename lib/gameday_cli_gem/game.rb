43615a14470f0dd100484252654b7e645cf6ceca
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-02-11 01:34:31 +0000

    stubbed out today method for game.rb

--- lib/gameday_cli_gem/game.rb
+++ lib/gameday_cli_gem/game.rb
@@ -3,6 +3,20 @@ class GamedayCliGem::Game
   attr_accessor :name, :team1, :team2, :start_time, :ongoing, :preview_url
 
   def self.today 
+    #should return all of today's games in an array
+    game_1 = self.new
+    game_1.name = "Heat vs Knicks"
+    game_1.start_time = "9:00pm"
+    game_1.ongoing = false
+    game_1.preview_url = "http://www.si.com/nhl/game/1646895"
+
+    game_2 = self.new
+    game_2.name = "Cavs vs Thunder"
+    game_2.start_time = "9:00pm"
+    game_2.ongoing = false
+    game_2.preview_url = "http://www.si.com/nhl/game/1646889"
+
+    [game_1, game_2]
   end
 
 end
